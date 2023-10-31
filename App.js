5acc1a241144ad320d895736c8299a6fe8443520
박진석 <103583674+jinDol99@users.noreply.github.com>
2023-10-31 16:58:09 +0900

    231031 Bottom Tab Navigation 컴포넌트 분리 및 디렉터리 구조 변경

--- App.js
+++ App.js
@@ -4,30 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// [23.10.30] Tab Navigation 동작 확인용 코드
-function HomeScreen() {
-  return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-  );
-}
-
-function SearchScreen() {
-  return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search Screen!</Text>
-      </View>
-  );
-}
+import HomeScreen from './src/screens/HomeScreen';
+import MyPageScreen from './src/screens/MyPageScreen';
+import SearchScreen from './src/screens/SearchScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -59,8 +38,8 @@ function MyTabs() {
                 ),
             }}/>
         <Tab.Screen
-            name="Settings"
-            component={SettingsScreen}
+            name="myPage"
+            component={MyPageScreen}
             options={{
                 title: '마이페이지',
                 tabBarIcon: ({color, size}) => (
@@ -78,22 +57,3 @@ export default function App() {
       </NavigationContainer>
   );
 }
-// [23.10.30] Tab Navigation 동작 확인용 코드 끝
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SearchScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>검색 화면입니당</Text>
+            <Text>검색 검색</Text>
+            <Text>영어로는 서치서치</Text>
+        </View>
+    );
+}
